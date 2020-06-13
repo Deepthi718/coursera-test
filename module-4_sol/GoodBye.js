679fd7615bd754829764276d01b4b4425e47fe36
Deepthi718 <bsdeepthi88@gmail.com>
2020-06-13 17:12:04 +0530

    Delete GoodBye.js

--- module-4_sol/GoodBye.js
+++
@@ -1,9 +0,0 @@
-(function(window){
-	var Byespeaker={};
-	var speakWord="Good Bye";
-
-	Byespeaker.speak=function (name) {
-   console.log(speakWord + " " + name);
-}
-window.Byespeaker=Byespeaker;
-})(window);
